ddecaa644684e4300fdd9db6873b9a44aaea9c79
AmjadTabani <muhammadamjadsohail786@gmail.com>
2025-12-19 15:23:15 +0500

    javascriptwork
    javascript chapter1-3

---
+++ javascript/chapter-1/app.js
@@ -0,0 +1,14 @@
+// ------- first line  alert --------------
+
+alert("Hello!")
+
+
+//-----------first & second line alert-------
+
+alert(" Javascript Alert \n \n welcome to JS land Happy Coding!")
+   
+
+//------------valid password ----------------
+
+alert("Error! enter valid password")
+ 
---
+++ javascript/chapter-2/app.js
@@ -0,0 +1,32 @@
+//------------alert name and surname -------------
+var come = "Welcome!"
+var Name = "Amjad"
+var surname = "Tabani"
+
+
+alert(come+Name+surname)
+
+
+//------------------student bio data ------------
+
+var studentname = "Amjad"
+var age ="18 year old"
+var certificate = "certified advance web development "
+
+alert (studentname)
+alert(age)
+alert(certificate)
+
+
+//------------------PIZZA------------------
+
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P" )     
+
+
+//---------------Email----------------
+
+
+var mail =" 894webdevrate@gmail.com"
+
+alert(mail)
+  
